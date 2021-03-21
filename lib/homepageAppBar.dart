dcc07295e1379199ad438d4baa36a06ef57386f1
Exusiai735 <2246834404@qq.com>
2021-03-21 20:52:10 +0800

    Update homepageAppBar.dart

--- lib/homepageAppBar.dart
+++ lib/homepageAppBar.dart
@@ -6,8 +6,10 @@ class HomepageAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      backgroundColor: Color(0xFF192433),
+      backgroundColor: Color.fromARGB(255, 25, 36, 51),
+      elevation: 20,
       title: Text('KZStats'),
+      centerTitle: true,
       actions: <Widget>[
         HomepageSearchBar(),
       ],
