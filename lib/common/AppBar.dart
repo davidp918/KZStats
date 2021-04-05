9ce5bba30d6b765e6fab57074e4cdc84f54af4a6
Exusiai <2246834404@qq.com>
2021-04-05 17:49:10 +0800

    Update AppBar.dart

--- lib/common/AppBar.dart
+++ lib/common/AppBar.dart
@@ -1,32 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:kzstats/cubit/cubit_update.dart';
 
+import 'package:kzstats/common/Popup_mode.dart';
 import 'SearchBar.dart';
 
 class HomepageAppBar extends StatelessWidget implements PreferredSizeWidget {
   final String currentPage;
   HomepageAppBar(this.currentPage);
-
-  static const modes = <String>[
-    'Kztimer',
-    'SimpleKZ',
-    'Vanilla',
-    'Pro',
-    'Nub',
-  ];
-
-  final List<PopupMenuItem<String>> _selections = modes
-      .map((String value) => PopupMenuItem<String>(
-            value: value,
-            child: Text(
-              value,
-              style: TextStyle(
-                color: Colors.white,
-              ),
-            ),
-          ))
-      .toList();
 
   @override
   Widget build(BuildContext context) {
@@ -37,40 +16,7 @@ class HomepageAppBar extends StatelessWidget implements PreferredSizeWidget {
       centerTitle: true,
       actions: <Widget>[
         HomepageSearchBar(),
-        PopupMenuButton(
-          color: Color(0xff4a5568),
-          offset: Offset(20, 26),
-          itemBuilder: (BuildContext context) => _selections,
-          onSelected: (String result) {
-            switch (result) {
-              case 'Kztimer':
-                {
-                  BlocProvider.of<ModeCubit>(context).kzt();
-                }
-                break;
-              case 'SimpleKZ':
-                {
-                  BlocProvider.of<ModeCubit>(context).skz();
-                }
-                break;
-              case 'Vanilla':
-                {
-                  BlocProvider.of<ModeCubit>(context).vnl();
-                }
-                break;
-              case 'Nub':
-                {
-                  BlocProvider.of<ModeCubit>(context).toNub();
-                }
-                break;
-              case 'Pro':
-                {
-                  BlocProvider.of<ModeCubit>(context).toPro();
-                }
-                break;
-            }
-          },
-        ),
+        PopUpModeSelect(),
       ],
     );
   }
