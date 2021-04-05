0360ff000aee8958c36788400c1ca69574af11c2
Exusiai <2246834404@qq.com>
2021-04-05 17:06:07 +0800

    Update urls.dart

--- lib/web/urls.dart
+++ lib/web/urls.dart
@@ -18,14 +18,14 @@ const kz_vanillaRecords =
 const kz_simpleTopRecords =
     "https://kztimerglobal.com/api/v2.0/records/top/recent?modes_list_string=kz_simple&place_top_at_least=1&has_teleports=false&stage=0&limit=20&tickrate=128";
 const kz_timerTopRecords =
-    "https://kztimerglobal.com/api/v2.0/records/top/recent?modes_list_string=kz_timer&place_top_at_least=1&has_teleports=false&stage=0&limit=60&tickrate=128";
+    "https://kztimerglobal.com/api/v2.0/records/top/recent?modes_list_string=kz_timer&place_top_at_least=1&has_teleports=false&stage=0&limit=20&tickrate=128";
 const kz_vanillaTopRecords =
     "https://kztimerglobal.com/api/v2.0/records/top/recent?modes_list_string=kz_vanilla&place_top_at_least=1&has_teleports=false&stage=0&limit=20&tickrate=128";
 
 const kz_simpleTopRecords_nub =
     "https://kztimerglobal.com/api/v2.0/records/top/recent?modes_list_string=kz_simple&place_top_at_least=1&stage=0&limit=20&tickrate=128";
 const kz_timerTopRecords_nub =
-    "https://kztimerglobal.com/api/v2.0/records/top/recent?modes_list_string=kz_timer&place_top_at_least=1&stage=0&limit=60&tickrate=128";
+    "https://kztimerglobal.com/api/v2.0/records/top/recent?modes_list_string=kz_timer&place_top_at_least=1&stage=0&limit=20&tickrate=128";
 const kz_vanillaTopRecords_nub =
     "https://kztimerglobal.com/api/v2.0/records/top/recent?modes_list_string=kz_vanilla&place_top_at_least=1&stage=0&limit=20&tickrate=128";
 
