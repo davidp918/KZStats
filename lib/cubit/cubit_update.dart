1e6837a598a3690174d3efcbe673c9654df3ff7a
Exusiai735 <2246834404@qq.com>
2021-04-06 11:17:31 +0800

    Update cubit_update.dart

--- lib/cubit/cubit_update.dart
+++ lib/cubit/cubit_update.dart
@@ -7,32 +7,32 @@ class ModeCubit extends Cubit<ModeState> {
 
   void kzt() {
     if (state.mode != 'Kztimer') {
-      emit(ModeState(mode: 'Kztimer'));
+      emit(ModeState(mode: 'Kztimer', nub: state.nub));
     }
   }
 
   void skz() {
     if (state.mode != 'SimpleKZ') {
-      emit(ModeState(mode: 'SimpleKZ'));
+      emit(ModeState(mode: 'SimpleKZ', nub: state.nub));
     }
   }
 
   void vnl() {
     if (state.mode != 'Vanilla') {
-      emit(ModeState(mode: 'Vanilla'));
+      emit(ModeState(mode: 'Vanilla', nub: state.nub));
     }
   }
 
   void toNub() {
     if (state.nub == false) {
       print('nub');
-      emit(ModeState(nub: true));
+      emit(ModeState(nub: true, mode: state.mode));
     }
   }
 
   void toPro() {
     if (state.nub == true) {
-      emit(ModeState(nub: false));
+      emit(ModeState(nub: false, mode: state.mode));
     }
   }
 }
