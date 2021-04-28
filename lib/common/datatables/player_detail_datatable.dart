1150efdb9e8065b94d2b944cd658e95cac9449ac
Exusiai <2246834404@qq.com>
2021-04-28 17:20:21 +0800

    Update player_detail_datatable.dart

--- lib/common/datatables/player_detail_datatable.dart
+++ lib/common/datatables/player_detail_datatable.dart
@@ -6,19 +6,19 @@ import 'package:kzstats/common/widgets/dataCells.dart';
 import 'package:kzstats/theme/colors.dart';
 import 'package:kzstats/web/json/record_json.dart';
 
-class BuildDataTable extends StatefulWidget {
+class PlayerDetailTable extends StatefulWidget {
   final List<Record>? records;
 
-  BuildDataTable({
+  PlayerDetailTable({
     Key? key,
     required this.records,
   }) : super(key: key);
 
   @override
-  _BuildDataTableState createState() => _BuildDataTableState();
+  _PlayerDetailTableState createState() => _PlayerDetailTableState();
 }
 
-class _BuildDataTableState extends State<BuildDataTable> {
+class _PlayerDetailTableState extends State<PlayerDetailTable> {
   late final List<Record>? _records;
   int? _sortColumnIndex;
   bool _isAscending = false;
@@ -75,13 +75,6 @@ class _BuildDataTableState extends State<BuildDataTable> {
 
   @override
   Widget build(BuildContext context) {
-    if (widget.tableType == 'map_detail') {
-      columns = map_detail_columns;
-    } else if (widget.tableType == 'player_detail') {
-      columns = player_detail_columns;
-    } else {
-      throw (UnimplementedError);
-    }
     return Theme(
       data: Theme.of(context).copyWith(
         dividerColor: dividerColor(),
@@ -106,7 +99,7 @@ class _BuildDataTableState extends State<BuildDataTable> {
         horizontalMargin: 12,
         columnSpacing: 15,
         columns: getColumns(columns),
-        source: RecordsSource(context, records, widget.tableType),
+        source: RecordsSource(context, records),
         sortColumnIndex: _sortColumnIndex,
         sortAscending: _isAscending,
       ),
@@ -117,40 +110,24 @@ class _BuildDataTableState extends State<BuildDataTable> {
 class RecordsSource extends DataTableSource {
   BuildContext context;
   List<Record>? _records;
-  final String tableType;
+
   RecordsSource(
     this.context,
     this._records,
-    this.tableType,
   );
   List<DataCell> selectDataCells(
     BuildContext context,
     int index,
     dynamic record,
-    String tableType,
   ) {
-    if (tableType == 'map_detail') {
-      return <DataCell>[
-        indexDataCell(index),
-        playerNameDataCell(context, record),
-        timeDataCell(record),
-        pointsDataCell(record),
-        teleportsDataCell(record),
-        createdOnDataCell(record),
-        serverNameDataCell(record),
-      ];
-    } else if (tableType == 'player_detail') {
-      return <DataCell>[
-        mapNameDataCell(context, record),
-        timeDataCell(record),
-        pointsDataCell(record),
-        teleportsDataCell(record),
-        createdOnDataCell(record),
-        serverNameDataCell(record),
-      ];
-    } else {
-      throw (UnimplementedError);
-    }
+    return <DataCell>[
+      mapNameDataCell(context, record),
+      timeDataCell(record),
+      pointsDataCell(record),
+      teleportsDataCell(record),
+      createdOnDataCell(record),
+      serverNameDataCell(record),
+    ];
   }
 
   @override
@@ -168,7 +145,7 @@ class RecordsSource extends DataTableSource {
           }
         },
       ),
-      cells: selectDataCells(context, index, record, tableType),
+      cells: selectDataCells(context, index, record),
     );
   }
 
