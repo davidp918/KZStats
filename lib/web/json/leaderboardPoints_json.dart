5dfa47048b726c93c5afddac448a93440150cae4
Exusiai <2246834404@qq.com>
2021-05-02 15:59:20 +0800

    Create leaderboardPoints_json.dart

---
+++ lib/web/json/leaderboardPoints_json.dart
@@ -0,0 +1,53 @@
+// To parse this JSON data, do
+//
+//     final leaderboardPoints = leaderboardPointsFromJson(jsonString);
+
+import 'dart:convert';
+
+List<LeaderboardPoints> leaderboardPointsFromJson(String str) =>
+    List<LeaderboardPoints>.from(
+        json.decode(str).map((x) => LeaderboardPoints.fromJson(x)));
+
+String leaderboardPointsToJson(List<LeaderboardPoints> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class LeaderboardPoints {
+  LeaderboardPoints({
+    this.points,
+    this.average,
+    this.rating,
+    this.finishes,
+    this.steamid64,
+    this.steamid,
+    this.playerName,
+  });
+
+  int? points;
+  double? average;
+  double? rating;
+  int? finishes;
+  String? steamid64;
+  String? steamid;
+  String? playerName;
+
+  factory LeaderboardPoints.fromJson(Map<String, dynamic> json) =>
+      LeaderboardPoints(
+        points: json["points"],
+        average: json["average"].toDouble(),
+        rating: json["rating"].toDouble(),
+        finishes: json["finishes"],
+        steamid64: json["steamid64"],
+        steamid: json["steamid"],
+        playerName: json["player_name"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "points": points,
+        "average": average,
+        "rating": rating,
+        "finishes": finishes,
+        "steamid64": steamid64,
+        "steamid": steamid,
+        "player_name": playerName,
+      };
+}
