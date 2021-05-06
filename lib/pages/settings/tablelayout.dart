591792ef4bfd9055daae109079927403162b1b01
Exusiai735 <2246834404@qq.com>
2021-05-06 23:04:49 +0800

    a

--- lib/pages/settings/tablelayout.dart
+++ lib/pages/settings/tablelayout.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:kzstats/global/responsive.dart';
 
 class SettingsTableLayout extends StatefulWidget {
@@ -18,7 +19,40 @@ class _SettingsTableLayoutState extends State<SettingsTableLayout> {
         return SingleChildScrollView(
           padding: EdgeInsets.all(30),
           child: Column(
-            children: [],
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'No. of Table Rows:',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+              Container(
+                child: TextFormField(
+                  decoration: InputDecoration(labelText: "Enter your number"),
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.digitsOnly
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: new TextFormField(
+                  maxLines: 1,
+                  decoration: new InputDecoration(
+                      labelText: 'Username',
+                      suffixIcon: new IconButton(
+                        highlightColor: Colors.transparent,
+                        icon: new Container(
+                            width: 36.0, child: new Icon(Icons.clear)),
+                        splashColor: Colors.transparent,
+                        onPressed: () {},
+                      ),
+                      prefixIcon: new Icon(Icons.account_circle)),
+                ),
+              ),
+            ],
           ),
         );
       },
