fc59b7d3d31332785782c017406410f27b62efea
Exusiai <2246834404@qq.com>
2021-05-28 17:39:44 +0800

    Update floater.dart

--- lib/common/widgets/floater.dart
+++ lib/common/widgets/floater.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
 
 class Floater extends StatefulWidget {
-  Floater({Key? key}) : super(key: key);
+  int amount;
+  Floater({Key? key, required this.amount}) : super(key: key);
 
   @override
   _FloaterState createState() => _FloaterState();
 }
 
 class _FloaterState extends State<Floater> {
+  late List<Animation> _animations;
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
